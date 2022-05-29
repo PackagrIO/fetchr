818c083b91fd6f9eb43708f5ea205bd83fec0bda
Jason Kulatunga <jason@thesparktree.com>
2022-05-29 08:53:47 -0700

    create provider interface

---
+++ pkg/provider/interface.go
@@ -0,0 +1,66 @@
+package provider
+
+import (
+	"github.com/package-url/packageurl-go"
+	"github.com/spf13/viper"
+	"io"
+	"net/http"
+)
+
+const (
+	ActionDownload    = "ActionDownload"
+	ActionSearch      = "ActionSearch"
+	ActionSetMetadata = "ActionSetMetadata"
+	ActionUpload      = "ActionUpload"
+)
+
+// Additional provider types should be added, matching those from this list:
+// https://github.com/package-url/purl-spec/blob/master/PURL-TYPES.rst
+const (
+	ProviderTypeDocker = "docker"
+	ProviderTypeGithub = "github"
+
+	// special provider type for storing files locally.
+	ProviderTypeLocal = "local"
+)
+
+// Create mock using:
+// mockgen -source=pkg/provider/interface.go -destination=pkg/provider/mock/mock_config.go
+type ProviderConfigInterface interface {
+	Get(key string) interface{}
+	GetString(key string) string
+	GetBool(key string) bool
+	GetInt(key string) int
+	GetInt32(key string) int32
+	GetInt64(key string) int64
+	GetUint(key string) uint
+	GetUint32(key string) uint32
+	GetUint64(key string) uint64
+	GetFloat64(key string) float64
+	GetStringMap(key string) map[string]interface{}
+	GetStringMapString(key string) map[string]string
+	GetStringMapStringSlice(key string) map[string][]string
+	GetSizeInBytes(key string) uint
+	UnmarshalKey(key string, rawVal interface{}, opts ...viper.DecoderConfigOption) error
+	Unmarshal(rawVal interface{}, opts ...viper.DecoderConfigOption) error
+	Set(key string, value interface{})
+	SetDefault(key string, value interface{})
+	MergeConfig(in io.Reader) error
+	AllKeys() []string
+}
+
+type ProviderInterface interface {
+	Init(providerName string, cfg ProviderConfigInterface, httpClient *http.Client) error
+	ValidateAuth() error
+	Close()
+
+	IsSupportedArtifactType(actionType string, artifactType string) bool
+
+	//given a single purl, may return multiple purl's (one for each sub artifact)
+	ArtifactSearch(artifactPurl *packageurl.PackageURL) ([]*packageurl.PackageURL, error)
+
+	ArtifactUpload(cachePath string, sourceArtifactPurl *packageurl.PackageURL, destArtifactPurl *packageurl.PackageURL) error
+	ArtifactDownload(basePath string, artifactPurl *packageurl.PackageURL) ([]string, []*packageurl.PackageURL, error)
+
+	ArtifactSetMetadata(metadata map[string]string, destArtifactPurl *packageurl.PackageURL) error
+}
